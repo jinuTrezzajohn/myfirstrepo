8182088e44f0b90257c48b4ee7747d425bb6a3e8
jinutrezzajohn6 <jinujohn685@gmail.com>
2023-07-30 14:17:13 +0530

    counted the number of vowels in list elements

--- tst.py
+++ tst.py
@@ -1,3 +1,11 @@
 l=['ABA','PYT','XYZ','MOM','NAN','HANNAH']
+c=0
+d={}
 for i in l:
-    print(i)
+    c=0
+    for j in range (len(i)):
+       if i[j] in ['A','E','I','O','U']:
+          c+=1
+    d[i]=c
+print(d)
+
